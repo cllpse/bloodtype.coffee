f21e3394bb6d1edb9f730d44638fd4368ba6d5be
Thomas Angelbo Christensen <thomas@eject.io>
2015-08-24 23:01:35 +0200

    More scaffolding and build process extravaganza!

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,27 @@
 
 
 var gulp = require("gulp");
+var runSequence = require("run-sequence");
 var uglify = require("gulp-uglify");
 var sass = require("gulp-sass");
 var minifyCss = require("gulp-minify-css");
 var minifyHTML = require("gulp-minify-html");
+var inlineSource = require("gulp-inline-source");
+var del = require("del");
 
 
-gulp.task("js", function ()
+gulp.task("js-compile", function ()
 {
-    gulp
+    return gulp
     .src("./js/index.js")
     .pipe(uglify())
     .pipe(gulp.dest("./dist"));
 });
 
 
-gulp.task("scss", function ()
+gulp.task("scss-compile", function ()
 {
-    gulp
+    return gulp
     .src("./scss/index.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(minifyCss())
@@ -27,18 +30,31 @@ gulp.task("scss", function ()
 });
 
 
-gulp.task("html", function ()
+gulp.task("html-compile", function ()
 {
-    gulp
+    return gulp
     .src("index.html")
     .pipe(minifyHTML())
+    .pipe(inlineSource())
     .pipe(gulp.dest("./dist"));
+});
+
+
+gulp.task("dist-clean-up", function ()
+{
+    del(["./dist/*.js", "./dist/*.css"]);
+});
+
+
+gulp.task("build", function ()
+{
+    runSequence("js-compile", "scss-compile", "html-compile", "dist-clean-up");
 });
 
 
 gulp.task("watch", function ()
 {
-    gulp.watch("./js/**/*.js", ["js"]);
-    gulp.watch("./scss/**/*.scss", ["scss"]);
-    gulp.watch("./index.html", ["html"]);
+    gulp.watch("./js/**/*.js", ["js-compile", "scss-compile", "html-compile"]);
+    gulp.watch("./scss/**/*.scss", ["js-compile", "scss-compile", "html-compile"]);
+    gulp.watch("./index.html", ["html-compile"]);
 });
