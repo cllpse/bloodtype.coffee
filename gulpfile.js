a43c98a57fbed9bb6755f29d764f60a9c691512c
Thomas Angelbo Christensen <thomas@eject.io>
2015-08-24 20:54:50 +0200

    Scaffolding

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,32 @@
-var gulp = require("gulp");
+"use strict";
 
-gulp.task("default", function ()
+
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var minifyCss = require("gulp-minify-css");
+var uglify = require("gulp-uglify");
+
+
+gulp.task("js", function ()
 {
+    gulp
+    .src("./js/**/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./dist"));
+});
+
+
+gulp.task("scss", function ()
+{
+    gulp
+    .src("./scss/**/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(minifyCss())
+    .pipe(gulp.dest("./dist"));
+});
+
+
+gulp.task("watch", function ()
+{
+    gulp.watch("./scss/**/*.scss", ["js", "scss"]);
 });
